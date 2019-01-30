09c7147458aa69708e82def577355bea6aa1f4d3
pav17 <pav17@hampshire.edu>
2019-01-30 14:47:34 -0500

    New unity file, old one had some wierd issues

--- Twitch Experimentation/Assets/Scripts/TwitchController.cs
+++ Twitch Experimentation/Assets/Scripts/TwitchController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TwitchLib.Client.Models;
 using TwitchLib.Unity;
 
 public class TwitchController : MonoBehaviour
 {
+
+    public Client client;
+    public string channelName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.runInBackground = true;
+
+        ConnectionCredentials credentials = new ConnectionCredentials("mrtwitchboto", Secrets.accessToken);
     }
 
     // Update is called once per frame
