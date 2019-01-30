6002fa9e1d15bff4c22dad1523af488b2132d7bd
pav17 <pav17@hampshire.edu>
2019-01-30 16:44:11 -0500

    updated scripts

---
+++ TwitchExperimentV2/Assets/Scripts/TwitchClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TwitchLib.Unity;
+using TwitchLib.Client.Models;
+using UnityEngine;
+
+public class TwitchClient : MonoBehaviour
+{
+
+    public Client client;
+    public string channelName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.runInBackground = true;
+
+        ConnectionCredentials credentials = new ConnectionCredentials('mrtwitchboto', Secrets.Instance.accessToken);
+        client = new Client();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
