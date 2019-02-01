a1ae535fe7eaa805da5cfb8a4a892a4a1f56670d
pav17 <pav17@hampshire.edu>
2019-02-01 13:57:15 -0500

    Cat movement implemented

--- HerdingChats/Assets/Scripts/CatMovement.cs
+++ HerdingChats/Assets/Scripts/CatMovement.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class CatMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody rb;
+    private float moveHorizontal;
+    private float moveVertical;
 
-    // Update is called once per frame
-    void Update()
+    void start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-
+    
     public void Move(string direction)
     {
         Debug.Log("Cat " + gameObject + " moved " + direction + "!");
+
+        if(direction == "Up" || direction == "up")
+        {
+            moveHorizontal = 0.0f;
+            moveVertical = 10.0f;
+        }
+        else if (direction == "Down" || direction == "down")
+        {
+            moveHorizontal = 0.0f;
+            moveVertical = -10.0f;
+        }
+        else if (direction == "Left" || direction == "left")
+        {
+            moveHorizontal = -10.0f;
+            moveVertical = 0.0f;
+        }
+        else if (direction == "Right" || direction == "right")
+        {
+            moveHorizontal = 10.0f;
+            moveVertical = 0.0f;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        
+        rb.AddForce(movement);
     }
 }
