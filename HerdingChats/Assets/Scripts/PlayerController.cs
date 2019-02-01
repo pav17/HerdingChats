6f1e53a3535a0fc17561e18ed6ee4c64b55e9b77
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2019-02-01 14:52:12 -0500

    Implemented player

---
+++ HerdingChats/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizotal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizotal, moveVertical, 0.0f);
+
+        //create Global
+        playerRB.velocity = movement * Global.Instance.playerSpeed;
+
+    }
+
+}
